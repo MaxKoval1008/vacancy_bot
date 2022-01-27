8c71c1f9eab148e90c32d5377e7ae31658c82b8d
Max Koval <max.koval.1008@gmail.com>
2022-01-27 15:42:06 +0300

    Main markups

--- handlers.py
+++ handlers.py
@@ -1,3 +1,30 @@
 from bot import dp
 from aiogram import types
 import keyboards as kb
+from privacy import privacy
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!", reply_markup=kb.markup_main)
+
+
+@dp.message_handler(text='Политика конфиденциальности')
+async def text_handler(message: types.Message):
+    await message.reply(privacy)
+
+
+@dp.message_handler(text='Cписок предложений')
+async def text_handler(message: types.Message):
+    await message.reply('Кнопка список предложений')
+
+
+@dp.message_handler(text='Мои объявления')
+async def process_command_1(message: types.Message):
+    await message.reply("Первая инлайн кнопка", reply_markup=kb.markup_3)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'Остановить')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(text='button_3_1')
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
--- keyboards.py
+++ keyboards.py
@@ -8,13 +8,21 @@ button_2 = KeyboardButton('Список вакансий')
 button_3 = KeyboardButton('Мои объявления')
 button_4 = KeyboardButton('Мои резюме')
 
-markup3 = ReplyKeyboardMarkup().insert(button_0)
-markup3.row(button_1, button_2)
-markup3.row(button_3, button_4)
+markup_main = ReplyKeyboardMarkup(
+    one_time_keyboard=False
+).insert(button_0)
+markup_main.row(button_1, button_2)
+markup_main.row(button_3, button_4)
+
+button_3_1 = InlineKeyboardButton('Остановить', callback_data='Остановить')
+button_3_2 = InlineKeyboardButton('Активировать', callback_data='Активировать')
+markup_3 = InlineKeyboardMarkup(row_width=2).insert(button_3_1)
+markup_3.insert(button_3_2)
+
+
 
 button_hi = KeyboardButton('Привет! 👋')
 
 greet_kb2 = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True
 ).add(button_hi)
-
